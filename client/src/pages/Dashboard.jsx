7cbad532c16bb88c3e96759a580ac8924816eee6
Kirti Kamal <kirtikamal02@gmail.com>
2024-08-22 11:37:59 +0530

    added delete post feature

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,23 +3,35 @@ import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import DashSidebar from "../components/DashSidebar";
 import DashProfile from "../components/DashProfile";
+import DashPosts from "../components/DashPosts";
 function Dashboard() {
   const location = useLocation();
-  const [tab, setTab] = useState("");
+  const [tab, setTab] = useState('');
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
-    const tabFromUrl = urlParams.get("tab");
-    console.log(tabFromUrl);
+    const tabFromUrl = urlParams.get('tab');
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
   }, [location.search]);
 
   return (
-    <div>
-      <div>{/* SideBar */}
-      <DashSidebar/>
-      </div>
-      {/* Profile */}
-      <DashProfile/>
+    <div className='min-h-screen flex flex-col md:flex-row'>
+    <div className='md:w-56'>
+      {/* Sidebar */}
+      <DashSidebar />
     </div>
+    {/* profile... */}
+    {tab === 'profile' && <DashProfile />}
+    {/* posts... */}
+    {tab === 'posts' && <DashPosts />}
+    {/* users */}
+    {tab === 'users' && <DashUsers />}
+    {/* comments  */}
+    {tab === 'comments' && <DashComments />}
+    {/* dashboard comp */}
+    {/* {tab === 'dash' && <DashboardComp />} */}
+  </div>
   );
 }
 
