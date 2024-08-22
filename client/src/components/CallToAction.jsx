adf8c930dfec377735fec178bb800b22e78061d0
Kirti Kamal <kirtikamal02@gmail.com>
2024-08-22 22:48:55 +0530

    change call to action image

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -17,7 +17,7 @@ export default function CallToAction() {
             </Button>
         </div>
         <div className="p-7 flex-1">
-            <img src="https://private-user-images.githubusercontent.com/98299441/360331833-c4397e5e-fce7-4521-b4bb-07c0c9e7d2f6.jpeg?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MjQzMTIzNzgsIm5iZiI6MTcyNDMxMjA3OCwicGF0aCI6Ii85ODI5OTQ0MS8zNjAzMzE4MzMtYzQzOTdlNWUtZmNlNy00NTIxLWI0YmItMDdjMGM5ZTdkMmY2LmpwZWc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwODIyJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDgyMlQwNzM0MzhaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT04OWI0ZmZkYzRhOWJiMWEyNDcyOGIxYmJjNjc3NDkxOGI5NzQwMjcyOGYxNjMyYTNmN2JjZDM2NGFkZmEwYjlmJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.grSUOyY6muza0We846kwZb0sM091RoTlaEZwas2SaBA" />
+            <img src="https://payu.in/blog/wp-content/uploads/2018/12/Blog-Cover2aa-1.png" />
         </div>
     </div>
   )
